b8a86b80cf7ef4d7635d59d57f0ef24526d69e3b
Gizele Santos <gizele.ccardoso@gmail.com>
2023-10-17 17:11:01 -0300

    module8: Change http methods to use the database and protect app

--- mulheres.js
+++ mulheres.js
@@ -1,6 +1,6 @@
 const express = require('express') //inicia express
-const { v4: uuidv4 } = require('uuid') //inicia uuid
 const router = express.Router() //configuração da primeira parte da rota
+const cors = require('cors') //trazendo o pacote cors que permite consumir essa api no frontend
 
 const conectBancoDeDados = require('./bancoDeDados')//ligando ao arquivo BancoDeDados
 conectBancoDeDados() //função que conecta o banco de dados
@@ -8,6 +8,8 @@ conectBancoDeDados() //função que conecta o banco de dados
 const Mulher = require('./mulherModel')
 const app = express() //inicia app
 app.use(express.json()) //prepara o dado que chegou via resquest para o formato json
+app.use(cors())
+
 const porta = 3333 //cria porta
 
 //GET
@@ -22,55 +24,60 @@ async function mostraMulheres(request, response) {
 }
 
 //POST
-function criaMulher(request, response) {
-  const novaMulher = {
-    id: uuidv4(),
+async function criaMulher(request, response) {
+  const novaMulher = new Mulher({
     nome: request.body.nome,
     imagem: request.body.imagem,
-    minibio: request.body.minibio
+    minibio: request.body.minibio,
+    citacao: request.body.citacao
+  })
+
+  try {
+    const mulherCriada = await novaMulher.save()
+    response.status(201).json(mulherCriada)
+  } catch (erro) {
+    console.log(erro)
   }
-
-  mulheres.push(novaMulher)
-
-  response.json(mulheres)
 }
 
 //PATCH
-function corrigeMulher(request, response) {
-  function encontraMulher(mulher) {
-    if (mulher.id === request.params.id) {
-      return mulher
+async function corrigeMulher(request, response) {
+  try {
+    const mulherEncontrada = await Mulher.findById(request.params.id)
+
+    if (request.body.nome) {
+      mulherEncontrada.nome = request.body.nome
     }
+
+    if (request.body.minibio) {
+      mulherEncontrada.minibio = request.body.minibio
+    }
+
+    if (request.body.imagem) {
+      mulherEncontrada.imagem = request.body.imagem
+    }
+
+    if (request.body.citacao) {
+      mulherEncontrada.citacao = request.body.citacao
+    }
+
+    const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+
+    response.json(mulherAtualizadaNoBancoDeDados)
+  } catch (erro) {
+    console.log(erro)
   }
-
-  const mulherEncontrada = mulheres.find(encontraMulher)
-
-  if (request.body.nome) {
-    mulherEncontrada.nome = request.body.nome
-  }
-
-  if (request.body.minibio) {
-    mulherEncontrada.minibio = request.body.minibio
-  }
-
-  if (request.body.imagem) {
-    mulherEncontrada.imagem = request.body.imagem
-  }
-
-  response.json(mulheres)
 }
 
 //DELETE
-function deletaMulher(request, response) {
-  function todasMenosEla(mulher) {
-    if(mulher.id !== request.params.id) {
-      return mulher
-    }
+async function deletaMulher(request, response) {
+  try {
+    await Mulher.findByIdAndDelete(request.params.id)
+
+    response.json({ message: 'Mulher deletada com sucesso!'})
+  } catch (erro) {
+    console.log(erro)
   }
-
-  const mulheresQueFicam = mulheres.filter(todasMenosEla)
-
-  response.json(mulheresQueFicam)
 }
 
 //PORTA
